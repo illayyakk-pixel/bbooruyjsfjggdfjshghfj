550d1e03f44456d9e5cf2b0145e2bec7d0b98e66
illayyakk-pixel <illayyakk@gmail.com>
2026-05-04 04:48:40 +0300

    script.js

--- script.js
+++ script.js
@@ -18,6 +18,24 @@ document.addEventListener("DOMContentLoaded", function () {
       .replace(/[\u0300-\u036f]/g, "");
   }
 
+  function transliterate(str) {
+  const map = {
+    а:"a", б:"b", в:"v", г:"g", д:"d",
+    е:"e", ё:"e", ж:"zh", з:"z", и:"i",
+    й:"y", к:"k", л:"l", м:"m", н:"n",
+    о:"o", п:"p", р:"r", с:"s", т:"t",
+    у:"u", ф:"f", х:"h", ц:"ts", ч:"ch",
+    ш:"sh", щ:"sht", ъ:"a", ы:"y", ь:"",
+    э:"e", ю:"yu", я:"ya"
+  };
+
+  return str
+    .toLowerCase()
+    .split("")
+    .map(c => map[c] || c)
+    .join("");
+}
+
   let companies = [];
 
   // 🔄 Loading message
